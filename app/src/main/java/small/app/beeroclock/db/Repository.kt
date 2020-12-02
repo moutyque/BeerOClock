209966ae6815d988a00fd68592ea165cd82b1089
Quentin <azfboom@gmail.com>
2020-12-02 22:52:22 +0100

    Fix bug due to DST Update them to handle dark mode Extract string from layout

--- app/src/main/java/small/app/beeroclock/db/Repository.kt
+++ app/src/main/java/small/app/beeroclock/db/Repository.kt
@@ -4,47 +4,40 @@ import android.content.Context
 import android.util.Log
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
-import java.time.zone.ZoneRulesException
 import java.util.*
-import kotlin.collections.ArrayList
 import kotlin.collections.HashMap
+import kotlin.math.abs
 
 
 class Repository(context: Context) {
     val db = getInstance(context)
-    val validZones: MutableList<String> = ArrayList()
     val validCities: MutableMap<City, String> = HashMap()
     private val tag = "Repository"
     private val availableIDs = TimeZone.getAvailableIDs()
     var currentTime = -1
-    suspend fun getCities() {
+    suspend fun getCities() = withContext(Dispatchers.IO) {
+        if (currentTime != Calendar.getInstance().get(Calendar.HOUR_OF_DAY)) {
+            currentTime = Calendar.getInstance().get(Calendar.HOUR_OF_DAY)
+            availableIDs.forEach {
+                //Some time zone are present in TimeZone but not usable by LocalDateTime class
+                try {
 
-
-        withContext(Dispatchers.IO) {
-            if (currentTime != Calendar.getInstance().get(Calendar.HOUR_OF_DAY)) {
-                currentTime = Calendar.getInstance().get(Calendar.HOUR_OF_DAY)
-                //TODO : Manage the availabelIds based on RAW offset + dst
-
-                availableIDs.forEach {
-                    //Some time zone are present in TimeZone but not usable by LocalDateTime class
-                    try {
-
-
-                        val hour = Calendar.getInstance(TimeZone.getTimeZone(it))
-                            .get(Calendar.HOUR_OF_DAY)
-                        if (hour == 18) {
-                            db.cityDAO().findCityInZone(it).forEach {
-                                validCities.put(it, db.countryDAO().findCountryName(it.county_code))
-                            }
+                    val instance = Calendar.getInstance(TimeZone.getTimeZone(it))
+                    val dst =
+                        instance.get(Calendar.DST_OFFSET)
+                    val offset =
+                        instance.get(Calendar.ZONE_OFFSET) / abs(instance.get(Calendar.ZONE_OFFSET))
+                    val hour = instance.get(Calendar.HOUR_OF_DAY)
+                    if ((hour + offset * dst) == 18) {
+                        db.cityDAO().findCityInZone(it).forEach {
+                            validCities.put(it, db.countryDAO().findCountryName(it.county_code))
                         }
-                    } catch (e: ZoneRulesException) {
-                        Log.e(tag, "The time zone $it do not exist")
                     }
+                } catch (e: Exception) {
+                    Log.e(tag, "The time zone $it do not exist")
                 }
             }
         }
-
-
     }
 
 }
